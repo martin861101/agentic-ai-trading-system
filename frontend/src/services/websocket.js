12189524e847375ec1b0416d882a52d648c8ecec
martin861101 <mschoeman3@gmail.com>
2025-08-01 16:39:31 +0200

    Update websocket.js

--- frontend/src/services/websocket.js
+++ frontend/src/services/websocket.js
@@ -1,1 +1,19 @@
-// WebSocket client
+// WebSocket clientexport class SignalWebSocket {
+  constructor(url) {
+    this.ws = new WebSocket(url);
+    this.ws.onopen = () => console.log("WebSocket connected");
+    this.ws.onclose = () => console.log("WebSocket disconnected");
+  }
+
+  onMessage(callback) {
+    this.ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      callback(data);
+    };
+  }
+
+  send(data) {
+    this.ws.send(JSON.stringify(data));
+  }
+}
+
